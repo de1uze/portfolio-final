6495ca135b586485a8ea8fa1bb8b86ca1e011dff
Pratik <shringarpure95@gmail.com>
2023-10-10 05:01:46 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    if (this.props.sharedBasicInfo) {
+      var networks = this.props.sharedBasicInfo.social.map(function (network) {
+        return (
+          <span key={network.name} className="m-4">
+            <a href={network.url} target="_blank" rel="noopener noreferrer">
+              <i className={network.class}></i>
+            </a>
+          </span>
+        );
+      });
+    }
+
+    return (
+      <footer>
+        <div className="col-md-12">
+          <div className="social-links">{networks}</div>
+
+          <div className="py-4 text-center">
+            <div className="container">
+              <small>
+                Contact Number - +91 9423583577
+              </small>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
